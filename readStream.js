37631aa4a757dfa357a55f6c6d94354be82be0a6
Emily Creamer <emily.creamer@icloud.com>
2019-04-09 09:25:57 -0700

    read stream

--- readStream.js
+++ readStream.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 
-const rs = fs.createReadStream('./1_streams.md');
-
-rs.on('data', data => {
-  console.log(data);
+const rs = fs.createReadStream('./1_streams.md', {
+  encoding: 'utf8',
+  highWaterMark: 64 * 1028
 });
+
+let body = '';
+rs.on('data', chunk => {
+  body += chunk;
+});
+
+rs.on('end', () => {
+  console.log(body);
+});
+
